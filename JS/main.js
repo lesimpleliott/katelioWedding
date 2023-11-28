7bdee69e09af9f75dc705f01173d78acf97adb79
eLeGarage <131270758+lesimpleliott@users.noreply.github.com>
2023-11-28 21:12:55 +0100

    Début HomePage

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,23 @@
+function toggleClass(element, className) {
+    element.classList.toggle(className);
+}
+
 function toggleMenu() {
+    // GESTION LORS DU CLICK SUR BURGER
     const navBar = document.querySelector('.navbar');
     const burgerButton = document.querySelector('.navbar__burger');
-
     burgerButton.addEventListener('click', () => {
-        navBar.classList.toggle('openNav')
-        burgerButton.classList.toggle('lineEffect')
+        toggleClass(navBar, 'openNav');
+        toggleClass(burgerButton, 'lineEffect');
+    })
+    
+    // GESTION DU CLICK POUR QUITTER LE MENU
+    const backLinks = document.querySelector('.navbar__links');
+    backLinks.addEventListener('click', (event) => {
+        if(!event.target.classList.contains('navbar__links__item')) {
+            toggleClass(navBar, 'openNav');
+            toggleClass(burgerButton, 'lineEffect');
+        }
     })
 }
 
