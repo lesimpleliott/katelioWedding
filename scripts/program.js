790feb51c48003894bdb74d310995011ed41538f
eLeGarage <131270758+lesimpleliott@users.noreply.github.com>
2024-01-15 21:09:08 +0100

    home V1 Done - need improve

--- scripts/program.js
+++ scripts/program.js
@@ -1,18 +1,14 @@
-
-
 const openDayFolder = (day, checkbox) => {
-    checkbox.addEventListener('change', function () {
+    checkbox.addEventListener("change", function () {
         if (this.checked) {
-            day.style.maxHeight = '5000px';
+            day.style.maxHeight = "5000px";
         } else {
-            day.style.maxHeight = '50px';
+            day.style.maxHeight = "50px";
         }
     });
+};
 
-}
-
-openDayFolder(friday, fridayCheckbox)
-openDayFolder(saturday, saturdayCheckbox)
-openDayFolder(sunday, sundayCheckbox)
-openDayFolder(monday, mondayCheckbox)
-
+openDayFolder(friday, fridayCheckbox);
+openDayFolder(saturday, saturdayCheckbox);
+openDayFolder(sunday, sundayCheckbox);
+openDayFolder(monday, mondayCheckbox);
