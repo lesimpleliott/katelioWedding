99b4cf8a73ff9d4576cb578b63713762ae82f25d
eLeGarage <131270758+lesimpleliott@users.noreply.github.com>
2024-01-16 16:02:45 +0100

    version Beta Done - improve ToDo

--- scripts/infos.js
+++ scripts/infos.js
@@ -1,23 +1,37 @@
-const cards = document.querySelectorAll('.card');
-const closeButtons = document.querySelectorAll('.closeCard');
+const displayCardsPlaces = () => {
+    const cards = document.querySelectorAll(".card");
+    const closeButtons = document.querySelectorAll(".closeCard");
+
+    // ouvre les cards
+    cards.forEach((card) => {
+        card.addEventListener("click", (e) => {
+            const id = e.target.closest(".card").id;
+            const card = document.getElementById(id);
+            card.classList.add("open");
+        });
+    });
+
+    // ferme les cards
+    closeButtons.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            e.stopPropagation();
+            const id = e.currentTarget.dataset.id;
+            const card = document.getElementById(id);
+            card.classList.remove("open");
+        });
+    });
+};
+displayCardsPlaces()
 
 
+const displayCardsTravel = () => {
+    const travelCards = document.querySelectorAll('.travelCard');
 
-// ouvre les cards
-cards.forEach(card => {
-    card.addEventListener('click', (e) => {
-        const id = e.target.closest('.card').id
-        const card = document.getElementById(id);
-        card.classList.add('open')
+    travelCards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('open')
+        })
     })
-})
 
-// ferme les cards
-closeButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.stopPropagation()
-        console.log(e.target)
-
-    })
-})
-
+}
+displayCardsTravel()
